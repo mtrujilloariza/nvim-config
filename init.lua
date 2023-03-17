b6f78719b3a0f04d17becf1dc09ba4c480a71586
mtrujilloariza <m1trujil@ucsd.edu>
2023-03-16 21:04:28 -0700

    inital commit

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- import options file
+require("mtrujilloariza.plugins-setup")
+require("mtrujilloariza.core.options")
+require("mtrujilloariza.core.keymaps")
+require("mtrujilloariza.core.colorscheme")
---
+++ lua/mtrujilloariza/core/options.lua
@@ -0,0 +1,33 @@
+
+-- line numbers
+vim.opt.number = true
+					
+-- tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.shiftround = true
+
+-- line wrap
+vim.opt.wrap = false
+
+-- remove auto comment
+vim.opt.formatoptions = "jclq" 
+
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- apperance
+vim.opt.termguicolors = true
+
+-- clipboard sync
+vim.opt.clipboard = "unnamedplus"
+
+-- mouse support 
+vim.opt.mouse = 'a'
+
+-- mouse setting
+vim.opt.scroll = 1
+
